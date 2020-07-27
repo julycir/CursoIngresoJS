c4d0880cb70ce6445dcb4c26df06e04ce6ad2bb2
julycir <julycir@gmail.com>
2020-07-26 21:51:55 -0300

    26 de julio 2020

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,4 +1,6 @@
-/*Enunciado: Las lámparas están al mismo precio de $35 pesos final.
+/*CIRAOLO JULIETA
+
+Enunciado: Las lámparas están al mismo precio de $35 pesos final.
 A. Si compra 6 o más lamparitas bajo consumo tiene un descuento del 50%.
 B. Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
 C. Si compra 4 lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
@@ -11,26 +13,57 @@ function CalcularPrecio ()
 	var precioLampara;
 	precioLampara = 35;
 	var cantidadDeLamparasCompradas;
-	cantidadDeLamparasCompradas = txtIdCantidad.value;
+	cantidadDeLamparasCompradas = parseInt(txtIdCantidad.value);
 	var marcaLampara;
 	marcaLampara = Marca.value;
 	var subTotal;
 	subTotal = cantidadDeLamparasCompradas * precioLampara;
 	var precioConDescuento;
-	precioConDescuento = txtIdprecioDescuento.value;
+	txtIdPrecioDescuento.value = precioConDescuento;
+	var iibb;
+	var precioTotal;
+	precioTotal = precioConDescuento + iibb;
 
-
-	//A. Si compra 6 o más lamparitas bajo consumo tiene un descuento del 50%.
-	if (cantidadDeLamparasCompradas > 5) {
-		precioConDescuento = subTotal * 0.5; 
-		console.log(precioConDescuento);
-		//A. FUNCIONA pero no sale en txtIdprecioDescuento.value
+	if (cantidadDeLamparasCompradas>=6) {
+		precioConDescuento = subTotal * 0.50;
+		console.log(precioConDescuento)
 	} else {
-		//B. Si compra 5 lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
-		if (cantidadDeLamparasCompradas === 5 && marcaLampara === "ArgentinaLuz") {
-			precioConDescuento = subTotal * 0.6; 
+		if (cantidadDeLamparasCompradas===5) {
+			if (marcaLampara==="ArgentinaLuz") {
+				precioConDescuento = subTotal * 0.60;
+			} else {
+				precioConDescuento = subTotal * 0.70;
+			}
 			console.log(precioConDescuento);
+		} else {
+			if (cantidadDeLamparasCompradas===4) {
+				if (marcaLampara==="ArgentinaLuz" || marcaLampara==="FelipeLamparas") {
+					precioConDescuento = subTotal * 0.75;
+				} else {
+					precioConDescuento = subTotal * 0.80;
+				}
+				console.log(precioConDescuento);
+			} else {
+				if (cantidadDeLamparasCompradas===3) {
+					if (marcaLampara==="ArgentinaLuz") {
+						precioConDescuento = subTotal * 0.85;
+					}
+					if (marcaLampara==="FelipeLamparas") {
+						precioConDescuento = subTotal * 0.90;
+					} else {
+						precioConDescuento = subTotal * 0.95;
+					}
+					console.log(precioConDescuento);
+				}
+ 			}
 		}
 	}
+
+	//tema IIBB
+	if (precioConDescuento>=120) {
+		iibb = precioConDescuento * 0.10;
+		alert("Usted pago " + iibb + " de IIBB.")
+	}
+	txtIdPrecioDescuento.value = precioTotal;
 }
 	
